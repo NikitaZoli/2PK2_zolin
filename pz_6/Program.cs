4a0ae0f61a2c77280f59ac50cbfea369bb16dbd5
zolin.na1687 <zolin.na1687@M409C-3R>
2022-09-29 09:00:51 +0300

    pz_5-6

---
+++ pz_6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double N, K, S;
+            Console.WriteLine("введите N (N>0):");
+            N = Convert.ToDouble(Console.ReadLine());
+            K = 1;
+            while (K <= N)
+            {
+                S = Math.Pow(2, K);
+                if (S == N)
+                Console.Write("{0}", K);               
+                K++;
+            }
+            Console.Read();
+
+                
+        }
+    }
+}
